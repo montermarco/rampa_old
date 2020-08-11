65bd7f1a1309bb6571bf337a10b1f8b82c327aa3
montermarco <monter.marco@gmail.com>
2020-08-10 19:22:36 -0500

    react-boiler-plate

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>
+      <h1>Happy codding!!!🔥</h1>
+    </div>
+  )
+}
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const HTMLwebpackplugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: path.join(__dirname, './index.js'),
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, './build')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 7000
+  },
+  plugins: [
+    new HTMLwebpackplugin({
+      filename: 'index.html',
+      template: './public/index.html'
+  })
+ ]
+}
